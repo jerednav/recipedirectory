a8fdd1e1cedbe82253821d78ee4c8f16f9155981
jerednav <80506833+jerednav@users.noreply.github.com>
2021-12-05 22:51:05 -0800

    Created context & provider, custom context hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
+import { useTheme } from "../hooks/useTheme";
 
 import Searchbar from "./Searchbar";
 
 export default function Navbar() {
+  const { color } = useTheme();
+
   return (
-    <div className='navbar'>
+    <div className='navbar' style={{ background: color }}>
       <nav>
         <Link to='/' className='brand'>
           <h1>Cooking Jred</h1>
