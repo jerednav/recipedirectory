e39a6b116c3ad09031bd9ddcfd28b5ac16817494
jerednav <80506833+jerednav@users.noreply.github.com>
2021-12-08 22:05:15 -0800

    realtime document data

--- src/pages/Recipe/Recipe.js
+++ src/pages/Recipe/Recipe.js
@@ -14,21 +14,28 @@ export default function Recipe() {
 
   useEffect(() => {
     setIsPending(true);
-  }, []);
 
-  projectFirestore
-    .collection("recipes")
-    .doc(id)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        setIsPending(false);
-        setRecipe(doc.data());
-      } else {
-        setIsPending(false);
-        setError("Could not find recipe");
-      }
+    const unsub = projectFirestore
+      .collection("recipes")
+      .doc(id)
+      .onSnapshot((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setRecipe(doc.data());
+        } else {
+          setIsPending(false);
+          setError("Could not find recipe");
+        }
+      });
+
+    return () => unsub();
+  }, [id]);
+
+  const handleClick = () => {
+    projectFirestore.collection("recipes").doc(id).update({
+      title: "Something completely different",
     });
+  };
 
   return (
     <div className={`recipe ${mode}`}>
@@ -44,6 +51,7 @@ export default function Recipe() {
             ))}
           </ul>
           <p className='method'>{recipe.method}</p>
+          <button onClick={handleClick}>Update Me</button>
         </>
       )}
     </div>
